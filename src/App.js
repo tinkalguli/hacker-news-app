14d1161bd4b90b613e373b458d0b6343ca8f2da7
tinkalguli <tinkalmunu@gmail.com>
2022-12-11 22:04:02 +0530

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import Dashboard from "./components/Dashboard";
+import NewsDetails from "./components/NewsDetails";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ToastContainer />
+      <Routes>
+        <Route exact path="/" element={<Dashboard />} />
+        <Route exact path="/:id" element={<NewsDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
